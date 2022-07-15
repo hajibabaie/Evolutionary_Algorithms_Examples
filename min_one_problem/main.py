a5d7cf024dfc2e5709d30d00fd8b3fdce08c8097
mo <hajibabaie.mohammad@gmail.com>
2022-07-15 12:58:50 +0200

    minOne problem with 600 binary variables. (best cost=3.0)

--- min_one_problem/main.py
+++ min_one_problem/main.py
@@ -7,19 +7,19 @@ def main():
 
     cost_func = cost_function
 
-    type_number_of_variables = {"binary1D": [250]}
+    type_number_of_variables = {"binary1D": [600]}
 
     solution_method = GA(cost_function=cost_func,
                          type_number_of_variables=type_number_of_variables,
-                         max_iteration=300,
-                         number_of_population=100,
+                         max_iteration=100,
+                         number_of_population=150,
                          parents_selection_method="roulette_wheel_selection",
                          selection_pressure_for_roulette_wheel=7,
                          tournament_size=2,
                          crossover_method="uniform",
-                         crossover_percentage=0.7,
+                         crossover_percentage=0.9,
                          mutation_percentage=0.4,
-                         mutation_rate=0.02,
+                         mutation_rate=0.03,
                          gamma_for_crossover=0.1,
                          gamma_for_mutation=0.1,
                          plot_cost_function=True)
