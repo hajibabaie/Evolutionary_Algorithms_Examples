0f58da126c1014229c6e6ea8f35fbe01bdcfbe85
mo <hajibabaie.mohammad@gmail.com>
2022-07-19 10:24:44 +0200

    minOne problem solved using genetic algorithm.

--- min_one_problem/min_one_problem.py
+++ min_one_problem/min_one_problem.py
@@ -3,6 +3,12 @@ import numpy as np
 
 def cost_function(x):
 
-    x = x["binary1D"][0]
+    for type_of_variable in x.keys():
+        if type_of_variable == "binary1D":
+            x = x["binary1D"][0]
+        elif type_of_variable == "real1D":
+            x = x["real1D"][0]
+            x[:, x[0, :] >= 0.5] = 1
+            x[:, x[0, :] < 0.5] = 0
 
     return x, float(np.sum(x, axis=1))
