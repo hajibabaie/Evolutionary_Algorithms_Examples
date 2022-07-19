5131cc179f41afe91c6afe10204282b257749a9d
mo <hajibabaie.mohammad@gmail.com>
2022-07-19 13:03:03 +0200

    transportation problem with fixed cost solved using particle swarm optimization.

--- transportation_problem_with_fixed_costs/main.py
+++ transportation_problem_with_fixed_costs/main.py
@@ -1,41 +1,64 @@
 from evolutionary_algorithms_examples.transportation_problem_with_fixed_costs.data import Data
 from evolutionary_algorithms_examples.transportation_problem_with_fixed_costs.transportation_problem_with_fixed_costs import TransportationProblemWithFixedCost
 from evolutionary_algorithms.genetic_algorithm import GA
+from evolutionary_algorithms.particle_swarm_optimization import PSO
 
 
 def main():
 
 
-    data = Data(number_of_suppliers=50,
-                number_of_customers=400,
-                customers_demand_min=100,
-                customers_demand_max=200)
-
-    data.create_and_save()
+    # data = Data(number_of_suppliers=50,
+    #             number_of_customers=400,
+    #             customers_demand_min=100,
+    #             customers_demand_max=200)
+    #
+    # data.create_and_save()
 
     model_data = Data.load()
 
+    # type_number_of_variables = {"real2D": [model_data["distances"].shape],
+    #                             "binary1D": [model_data["suppliers_fixed_cost"].shape[1]]}
+
+
     type_number_of_variables = {"real2D": [model_data["distances"].shape],
-                                "binary1D": [model_data["suppliers_fixed_cost"].shape[1]]}
+                                "real1D": [model_data["suppliers_fixed_cost"].shape[1]]}
 
     problem = TransportationProblemWithFixedCost()
     cost_func = problem.cost_function
 
-    solution_method = GA(cost_function=cost_func,
-                         type_number_of_variables=type_number_of_variables,
-                         min_range_of_real_variables=0,
-                         max_range_of_real_variables=1,
-                         max_iteration=100,
-                         number_of_population=80,
-                         parents_selection_method="roulette_wheel_selection",
-                         selection_pressure_for_roulette_wheel=7,
-                         crossover_method="uniform",
-                         crossover_percentage=0.7,
-                         mutation_percentage=0.4,
-                         mutation_rate=0.02,
-                         gamma_for_crossover=0.3,
-                         gamma_for_mutation=0.3,
-                         plot_cost_function=True)
+    # solution_method = GA(cost_function=cost_func,
+    #                      type_number_of_variables=type_number_of_variables,
+    #                      min_range_of_real_variables=0,
+    #                      max_range_of_real_variables=1,
+    #                      max_iteration=100,
+    #                      number_of_population=80,
+    #                      parents_selection_method="roulette_wheel_selection",
+    #                      selection_pressure_for_roulette_wheel=7,
+    #                      crossover_method="uniform",
+    #                      crossover_percentage=0.7,
+    #                      mutation_percentage=0.4,
+    #                      mutation_rate=0.02,
+    #                      gamma_for_crossover=0.3,
+    #                      gamma_for_mutation=0.3,
+    #                      plot_cost_function=True)
+    #
+    # solution_best, run_time = solution_method.run()
+    #
+    # return solution_best, run_time
+
+    solution_method = PSO(cost_function=cost_func,
+                          type_number_of_variables=type_number_of_variables,
+                          min_range_of_variables=0,
+                          max_range_of_variables=1,
+                          max_iteration=100,
+                          number_of_particles=50,
+                          inertia_rate=0.7298,
+                          inertia_damping_rate=1,
+                          personal_learning_rate=1.4962,
+                          global_learning_rate=1.4962,
+                          tempo_limit=True,
+                          plot_cost_function=True,
+                          mutation_for_real=True)
 
     solution_best, run_time = solution_method.run()
 
--- transportation_problem_with_fixed_costs/transportation_problem_with_fixed_costs.py
+++ transportation_problem_with_fixed_costs/transportation_problem_with_fixed_costs.py
@@ -1,3 +1,5 @@
+import copy
+
 from evolutionary_algorithms_examples.transportation_problem_with_fixed_costs.data import Data
 import numpy as np
 
@@ -26,13 +28,38 @@ class TransportationProblemWithFixedCost:
         suppliers_fixed_cost = self._model_data["suppliers_fixed_cost"]
         distances = self._model_data["distances"]
 
-        x_real = x["real2D"][0]
-        x_binary = x["binary1D"][0]
+        for type_of_variables in x.keys():
 
-        x_parsed = self._parse_solution(x_real, x_binary)
+            if type_of_variables == "real2D":
+
+                x_real2D = x["real2D"][0]
+
+            if type_of_variables == "binary1D":
+                x_binary1D = x["binary1D"][0]
+
+            if type_of_variables == "real1D":
+                x_real1D = x["real1D"][0]
+                x_real1D = copy.deepcopy(x_real1D)
+                x_real1D[:, x_real1D[0, :] >= 0.5] = 1
+                x_real1D[:, x_real1D[0, :] < 0.5] = 0
+
+        if "binary1D" in x.keys():
+
+            x_parsed = self._parse_solution(x_real2D, x_binary1D)
+
+        if "real1D" in x.keys():
+            x_parsed = self._parse_solution(x_real2D, x_real1D)
 
         objective_function_real_part = np.sum(np.multiply(x_parsed, distances))
-        objective_function_binary_part = np.dot(x_binary, suppliers_fixed_cost.T)[0, 0]
+
+        if "binary1D" in x.keys():
+
+            objective_function_binary_part = np.dot(x_binary1D, suppliers_fixed_cost.T)[0, 0]
+
+        if "real1D" in x.keys():
+
+            objective_function_binary_part = np.dot(x_real1D, suppliers_fixed_cost.T)[0, 0]
+
         objective_function = objective_function_real_part + objective_function_binary_part
 
         suppliers_sent = np.sum(x_parsed, axis=1, keepdims=True)
